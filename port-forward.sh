4d46168ebd2081f19b87b33d5897b6e79423f662
Ulf Slunga <ulf.slunga@sinetiq.se>
2025-12-10 08:36:44 +0100

    wip

--- port-forward.sh
+++ port-forward.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "Finding service matching with '-vah'"
-VAH_SERVICE=$(kubectl get svc -n default --no-headers -o custom-columns=":metadata.name" | grep -- '-vah$' | head -n 1 | tr -d '[:space:]')
-echo "VAH_SERVICE='$VAH_SERVICE'"
-if [ -n "$VAH_SERVICE" ]; then
-  echo "Starting port-forward: kubectl port-forward service/$VAH_SERVICE 8888:8080"
+echo "Finding service matching with '-kundbehovsflode'"
+KUNDBEHOVSFLODE_SERVICE=$(kubectl get svc -n default --no-headers -o custom-columns=":metadata.name" | grep -- '-kundbehovsflode$' | head -n 1 | tr -d '[:space:]')
+echo "KUNDBEHOVSFLODE_SERVICE='$KUNDBEHOVSFLODE_SERVICE'"
+if [ -n "$KUNDBEHOVSFLODE_SERVICE" ]; then
+  echo "Starting port-forward: kubectl port-forward service/$KUNDBEHOVSFLODE_SERVICE 8888:8080"
   echo "Sleeping 30 sec making sure that application is responding before forwarding"
   sleep 30
-  nohup kubectl port-forward service/"$VAH_SERVICE" 8888:8080 > portforward.log 2>&1 &
+  nohup kubectl port-forward service/"$KUNDBEHOVSFLODE_SERVICE" 8888:8080 > portforward.log 2>&1 &
   echo $! > portforward.pid
 else
-  echo "No service ending with '-vah' found — skipping port-forward."
+  echo "No service ending with '-kundbehovsflode' found — skipping port-forward."
 fi

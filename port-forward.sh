567350dbd8a7e7e0faf6c262b43ce8ea995155b4
Ulf Slunga <ulf.slunga@sinetiq.se>
2025-12-23 07:40:32 +0100

    fix: exponerar port 5005 för containers, möjliggör remote debugging

--- port-forward.sh
+++ port-forward.sh
@@ -25,7 +25,7 @@ fi
 
 echo "Finding service matching with '-rtf-manuell'"
 RTF_MANUELL_SERVICE=$(kubectl get svc -n default --no-headers -o custom-columns=":metadata.name" | grep -- '-rtf-manuell' | head -n 1 | tr -d '[:space:]')
-echo "RTF_MANUELL='$RTF_MANUELL_SERVICE'"
+echo "RTF_MANUELL_SERVICE='$RTF_MANUELL_SERVICE'"
 if [ -n "$RTF_MANUELL_SERVICE" ]; then
   echo "Starting port-forward: kubectl port-forward service/$RTF_MANUELL_SERVICE 8890:8080"
   nohup kubectl port-forward service/"$RTF_MANUELL_SERVICE" 8890:8080 > portforward_rtf_manuell.log 2>&1 &
@@ -37,3 +37,14 @@ fi
 # Port forwarding to kafka external nodeport listener
 echo "Starting port-forward: kubectl port-forward svc/dev-kafka-dev-kafka-combined-0 9094:9094"
 nohup kubectl port-forward svc/dev-kafka-dev-kafka-combined-0 9094:9094 >> portforward.log 2>&1 &
+
+echo "Finding pod matching '-rtf-manuell'"
+RTF_MANUELL_POD=$(kubectl get pod -n default --no-headers -o custom-columns=":metadata.name"| grep -- '-rtf-manuell')
+echo "RTF_MANUELL_POD='$RTF_MANUELL_POD'"
+if [ -n "$RTF_MANUELL_POD" ]; then
+  echo "Starting port-forward: kubectl port-forward pod/RTF_MANUELL_POD 5005:5005"
+  nohup kubectl port-forward pod/"$RTF_MANUELL_POD" 5005:5005 > portforward_rtf_manuell_debug.log 2>&1 &
+  echo $! > portforward_rtf_manuell_debug.pid
+else
+  echo "No pod matching '-rtf-manuell' found — skipping port-forward."
+fi
